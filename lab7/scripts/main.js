de879e829354a9a73b70f5bf8d15497d059ab439
RodrigoDias04 <160158472+RodrigoDias04@users.noreply.github.com>
2024-11-06 21:31:18 +0000

    5 eventos a funcionar

--- lab7/scripts/main.js
+++ lab7/scripts/main.js
@@ -1,3 +1,5 @@
+
+//evento1
 const frase = document.querySelector('#mensagem');
 
 function mudaFrase() {
@@ -14,10 +16,50 @@ function restauraFrase() {
 frase.addEventListener('mouseover', mudaFrase);
 frase.addEventListener('mouseout', restauraFrase);
 
-const pintaMe = document.querySelector('#pinta-me');
-const colorButtons = document.querySelector('.color-button'); 
-function alteracor(){
-    
-}
-pintaMe.addEventListener('click', alteracor);
+//evennto2
+const pintaMe = document.querySelector("#pinta-me");
+const colorButtons = document.querySelectorAll(".color-button");
 
+function alteraCor(event) {
+  const color = event.target.innerText.toLowerCase();
+  pintaMe.style.color = color;
+}
+
+colorButtons.forEach((button) => {
+  button.addEventListener("click", alteraCor);
+});
+
+//evento3
+const texto = document.querySelector("#caixatexto1");
+const cores = ["red", "green", "blue", "yellow", "gray"];
+let colorIndex = 0;
+
+function alteraCorBackground() {
+  texto.style.backgroundColor = cores[colorIndex];
+  colorIndex = (colorIndex + 1) % cores.length;
+}
+
+texto.addEventListener("input", alteraCorBackground);
+texto.addEventListener("change", alteraCorBackground);
+
+//evento4
+const textoBackground = document.querySelector("#caixatexto2");
+const submitButton = document.querySelector("#submeter2");
+
+function alteraBackground() {
+  const color = textoBackground.value;
+  document.body.style.backgroundColor = color;
+}
+
+submitButton.addEventListener("click", alteraBackground);
+
+//evento5
+
+const butao = document.querySelector(".contador");
+const numero = document.querySelector("#numero");
+
+function aumentaNumero() {
+  numero.innerText = Number(numero.innerText) + 1;
+}
+
+butao.addEventListener("click", aumentaNumero);
